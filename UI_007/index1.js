91a3acf1ec66c94b09f58c663e1dcf94544c6ba3
loo919239 <loo919239@naver.com>
2024-01-08 23:38:08 +0900

    UIUX

---
+++ UI_007/index1.js
@@ -0,0 +1,16 @@
+/*
+DONComtemt:oaded : 화면에 Dom 요소들이 모두 완성되면...
+이 event 를 설정하므로 문서 (DOM)
+*/
+
+// document.addEventListener{"DOMContentLoaded", () => {
+//     /*  화면상에서 li tag 가 여러개 있는데 querySelector() 를 사용하여 요청을 하면 화면에서 최초로 발견되는 요소만 seclect 가 된다*/
+//     const liContent = document.querySelector("section.list li");
+//     liContent.addEventListener("click", (event) => {
+
+//         const target = event.target
+//         if(targ)
+
+
+//     });
+// }};
